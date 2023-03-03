30fa6111960d3f29e1393fa1655675a77da0e3f7
aweaver <aweaver@student.42.fr>
2023-03-03 19:16:33 +0100

    ADD DOCKERFILE FOR MARIADB:
    Adding dockerfile and initialisation script for mariadb.
    This is without passwords, passwords need to be added in the script.
    Otherwise they will work just fine from the .env file when launched with
    the docker compose script.

---
+++ srcs/mariadb/init_database.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
+chown -R mysql:mysql /var/lib/mysql
+
+MYSQL_DATABASE=aweaver
+MYSQL_ROOT_LOGIN=staf
+MYSQL_USER=just_a_user
+
+tfile=`mktemp`
+if [ ! -f "$tfile" ]; then
+    return 1
+fi
+cat << EOF > $tfile
+
+DROP DATABASE IF EXISTS test ;
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '"$MYSQL_ROOT_LOGIN"'@'%' IDENTIFIED BY '"$MYSQL_ROOT_PASSWORD"';
+CREATE USER IF NOT EXISTS '"$MYSQL_USER"'@'%' IDENTIFIED BY '"$MYSQL_PASSWORD"';
+GRANT ALL PRIVILEGES ON '"$MYSQL_DATABASE"' TO '"$MYSQL_ROOT_LOGIN"'@'%';
+ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD"';
+FLUSH PRIVILEGES;
+EOF
+
+/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
+rm -f $tfile
+
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0
