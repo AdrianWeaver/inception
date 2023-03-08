b2acb5bc5e011123f11d5b566d943c2e61ca9254
aweaver <aweaver@student.42.fr>
2023-03-08 16:24:58 +0100

    ADD WORDPRESS:
    Adding wordpress.
    Still need to fix issues with users in separated containers.

---
+++ srcs/wordpress/wp-config.php
@@ -0,0 +1,16 @@
+<?php
+define( 'DB_NAME', 'aweaver' );
+define( 'DB_USER', 'mysql' );
+define( 'DB_PASSWORD', 'newpassword' );
+define( 'DB_HOST', 'localhost:3306' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+require_once ABSPATH . 'wp-settings.php';
