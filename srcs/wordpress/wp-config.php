afd1ca140478647f77e4623f89e6323de335e99a
aweaver <aweaver@student.42.fr>
2023-04-25 11:32:34 +0200

    UPDATE CONF FILE (WORDPRESS):
    wp-config.php now includes domain name url and homepage.

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -3,6 +3,8 @@ define( 'DB_NAME', "'$MYSQL_DATABASE'" );
 define( 'DB_USER', "'$MYSQL_ROOT_USER'" );
 define( 'DB_PASSWORD', "'$MYSQL_ROOT_PASSWORD'" );
 define( 'DB_HOST', "'$MYSQL_HOST'" );
+define( 'WP_HOME', "https://'$DOMAIN_NAME'" );
+define( 'WP_SITEURL', "https://'$DOMAIN_NAME/wordpress'" );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
