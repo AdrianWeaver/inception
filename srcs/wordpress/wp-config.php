66ba13a52615356ddb66cd278acf527212c3ad01
aweaver <aweaver@student.42.fr>
2023-04-28 20:16:56 +0200

    FIX ROOT DIRECTORY CONF (WORDPRESS):
    Fixing WP_SITEURL line of the wp-config.php file.
    Since the nginx server has its root directory set as
    /var/www/html/wordpress it is not needed to include /wordpress at the
    end of the WP_SITEURL definition.

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -4,7 +4,7 @@ define( 'DB_USER', "'$MYSQL_ROOT_USER'" );
 define( 'DB_PASSWORD', "'$MYSQL_ROOT_PASSWORD'" );
 define( 'DB_HOST', "'$MYSQL_HOST'" );
 define( 'WP_HOME', "https://'$DOMAIN_NAME'" );
-define( 'WP_SITEURL', "https://'$DOMAIN_NAME/wordpress'" );
+define( 'WP_SITEURL', "https://'$DOMAIN_NAME'" );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
