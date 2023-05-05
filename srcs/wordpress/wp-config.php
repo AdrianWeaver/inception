3373dbca506372a99717b53a738215cb2fcbd275
aweaver <aweaver@student.42.fr>
2023-05-05 17:51:36 +0200

    WIP WP-CONFIG (WORDPRESS):
    This is a WIP commited to work from home.
    Trying to figure out if there's an issue with quotes and variables
    expansion.
    This could be the issue causing the database to not be detected, leading
    to a wp config prompt.

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -1,10 +1,10 @@
 <?php
-define( 'DB_NAME', "'$MYSQL_DATABASE'" );
-define( 'DB_USER', "'$MYSQL_ROOT_USER'" );
-define( 'DB_PASSWORD', "'$MYSQL_ROOT_PASSWORD'" );
-define( 'DB_HOST', "'$MYSQL_HOST'" );
-define( 'WP_HOME', "https://'$DOMAIN_NAME'" );
-define( 'WP_SITEURL', "https://'$DOMAIN_NAME'" );
+define( 'DB_NAME', "$MYSQL_DATABASE" );
+define( 'DB_USER', "$MYSQL_ROOT_USER" );
+define( 'DB_PASSWORD', "$MYSQL_ROOT_PASSWORD" );
+define( 'DB_HOST', "$MYSQL_HOST" );
+define( 'WP_HOME', "https://$DOMAIN_NAME" );
+define( 'WP_SITEURL', "https://$DOMAIN_NAME" );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
