10501b44e3ba2a4cf396b39b8a06a51dd4caf751
aweaver <aweaver@student.42.fr>
2023-05-10 18:22:54 +0200

    FIX USERS (WORDPRESS):
    Now creating users as intended.

--- srcs/wordpress/wp_entryscript.sh
+++ srcs/wordpress/wp_entryscript.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-set -x
 sleep 10
 if [ -f /var/www/html/wordpress/wp-config.php ]
 then
 	exec /usr/sbin/php-fpm81 -F -R
 else
+	echo "create"
 	wp config create --allow-root --dbname=$DATABASE_NAME --dbuser=$DATABASE_LOGIN --dbpass=$DATABASE_PASSWORD --dbhost=mariadb:3306 --path=/var/www/html/wordpress/
-	wp core install --url=$NGINX_HOST --title=Inception --admin_user=$WP_ADMIN --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --path=/var/www/html/wordpress
-	wp user create --user-login=$WP_ADMIN --user_pass=$WP_ADMIN_PASSWORD --role=administrator --display_name=administrator
-	wp user create --user-login=$WP_USER --user_pass=$WP_USER_PASSWORD --role=subscriber --display_name=$WP_USER
+	echo "install"
+	wp core install --url=$NGINX_HOST --title=Inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html/wordpress --skip-email
+	echo "creating user"
+	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=subscriber --display_name=$WP_USER --porcelain --path=/var/www/html/wordpress
 	exec /usr/sbin/php-fpm81 -F -R
 fi
-
-set +x
