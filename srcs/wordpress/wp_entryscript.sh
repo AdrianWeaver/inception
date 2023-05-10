7f746b66fd39f0b3f2c50acefb2a670d6a284f75
aweaver <aweaver@student.42.fr>
2023-05-10 20:23:57 +0200

    UPDATE HEALTHCHECK (MARIADB):
    Now checking for database health instead of using a dirty sleep.
    
    Also note the docker-compose.yml is mandatory for the subject, a
    compose.yml file should be the norm now.

--- srcs/wordpress/wp_entryscript.sh
+++ srcs/wordpress/wp_entryscript.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-sleep 10
 if [ -f /var/www/html/wordpress/wp-config.php ]
 then
 	exec /usr/sbin/php-fpm81 -F -R
 else
-	echo "create"
 	wp config create --allow-root --dbname=$DATABASE_NAME --dbuser=$DATABASE_LOGIN --dbpass=$DATABASE_PASSWORD --dbhost=mariadb:3306 --path=/var/www/html/wordpress/
-	echo "install"
 	wp core install --url=$NGINX_HOST --title=Inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html/wordpress --skip-email
-	echo "creating user"
 	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=subscriber --display_name=$WP_USER --porcelain --path=/var/www/html/wordpress
 	exec /usr/sbin/php-fpm81 -F -R
 fi
